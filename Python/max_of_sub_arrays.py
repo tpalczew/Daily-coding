5eb76dc98f9216dfe9168db5e1aad4d5ae140809
Tomasz Palczewski <tomasz.palczewski@gmail.com>
2022-09-21 20:43:15 -0700

    Update max_of_sub_arrays.py

--- Python/max_of_sub_arrays.py
+++ Python/max_of_sub_arrays.py
@@ -22,8 +22,6 @@ You can simply print them out as you compute them.
 
 '''
 
-arr = [10, 5, 2, 7, 8, 7] 
-
 def solution(arr, k):
     n = len(arr)
     result = []
@@ -38,6 +36,8 @@ def solution(arr, k):
     return result
 
 # --- 
+
+arr = [10, 5, 2, 7, 8, 7] 
 k = 3
 print( solution(arr, k) )
 
@@ -49,6 +49,31 @@ Dequeu
 '''
 
 
+from collections import deque
 
 
+def solution(arr, k):
+    result = []
+    q = deque()
+    for i in range(k):
+        while q and arr[i] >= arr[q[-1]]:
+            q.pop()
+        q.append(i)
+
+    for i in range(k, len(arr)):
+        print(arr[q[0]])
+        result.append(arr[q[0]])
+        while q and q[0] <= i-k:
+            q.popleft()
+        while q and arr[i] >= arr[q[-1]]:
+            q.pop()
+        q.append(i)
+    print(arr[q[0]])
+    result.append(arr[q[0]])
+    return result
+
+
+arr = [10, 5, 2, 7, 8, 7] 
+k = 3
+print( solution(arr, k) )
 
